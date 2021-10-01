30da9fe3a078e59f9b771fabbafa90a56b86660a
CirCu1tI0N3rd <thanhhaidoan@gmail.com>
2021-10-01 18:58:45 +0700

    Updated LICENSE Updated CMake profiles and tree

--- include/stinfopp/exceptions.h
+++ include/stinfopp/exceptions.h
@@ -1,16 +1,38 @@
-#pragma once
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Circu1tI0N3rd (thanhhaidoan@tutanota.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef _STINFO_EXCEPT_H
 #define _STINFO_EXCEPT_H 1
 
-#include "stinfopp_global.h"
 #include <exception>
 #include <string>
 #include <curl/curl.h>
 
 namespace stinfo_e {
-	CURLcode STINFOPP_EXPORT curl_assert(const char errBuf[], CURLcode resp, bool justThrow = false);
+    CURLcode curl_assert(const char errBuf[], CURLcode resp, bool justThrow = false);
 
-	class STINFOPP_EXPORT CURLerror : public std::exception {
+    class CURLerror : public std::exception {
 	private:
 		CURLcode errCode;
 		std::string msg;
--- include/stinfopp/stinfopp.h
+++ include/stinfopp/stinfopp.h
@@ -1,7 +1,30 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Circu1tI0N3rd (thanhhaidoan@tutanota.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef STINFOPP_H
 #define STINFOPP_H
 
-#include "stinfopp_global.h"
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -14,13 +37,13 @@
 #define HEADER_ACCEPT_JSON		"Accept: text/html,application/xhtml+xml,application/xml,application/json"
 #define HEADER_CONTENT_TYPE_P   "Content-Type: application/json; charset=UTF-8"
 
-struct STINFOPP_EXPORT postDataPack
+struct postDataPack
 {
     std::string data;
     size_t pos;
 };
 
-class STINFOPP_EXPORT stinfo
+class stinfo
 {
 private:
     char errorBuffer[CURL_ERROR_SIZE];
